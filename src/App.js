7ea379bd8956d40c8937d01eb9090e43a3447c30
TuanDat <letuandat556@gmail.com>
2024-01-01 09:26:39 +0700

    fix bugs in merging

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ function App() {
                         <Route path="/manager" element={<TableManager />} />
                     </Routes>
                 </Container>
+                <Footer />
             </div>
             <ToastContainer
                 position="top-right"
